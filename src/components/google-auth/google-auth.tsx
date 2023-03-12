f13c259172d26c2fb93a147e58650be041d49723
rafaeldinov <40115049+rafaeldinov@users.noreply.github.com>
2023-03-12 21:38:31 +0200

    all done

--- src/components/google-auth/google-auth.tsx
+++ src/components/google-auth/google-auth.tsx
@@ -1,11 +1,14 @@
 import { GoogleLogin } from '@react-oauth/google';
+import jwt_decode from 'jwt-decode'; // eslint-disable-line
 
 export default function GoogleAuth() {
   return (
     <div>
       <GoogleLogin
         onSuccess={(credentialResponse) => {
-          console.log(credentialResponse);
+          if(credentialResponse.credential) {
+            console.log(jwt_decode(credentialResponse.credential)); // eslint-disable-line
+          }
         }}
         onError={() => {
           console.log('Login Failed');
